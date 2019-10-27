8202e49c4d8762d739897830fca9c6006a0847ae
Freniel <frenielzabala@gmail.com>
2019-10-26 18:21:53 -0700

    docusgin tools to use

--- backend/docusgn_tooling.py
+++ backend/docusgn_tooling.py
@@ -1,183 +1,131 @@
-""" Example 002: Remote signer, cc, envelope has three documents """
-
-from flask import render_template, url_for, redirect, session, flash, request
+import os
 from os import path
 import base64
 import re
 import json
 from docusign_esign import *
-from docusign_esign.client.api_exception import ApiException
+from dotenv import load_dotenv
+from datetime import datetime,timedelta
+#loading environment variables
+load_dotenv()
 
-demo_docs_path = path.abspath(path.join(path.dirname(path.realpath(__file__)), "static/documents"))
+#get path to document
+docs_path = path.abspath(path.join(path.dirname(path.realpath(__file__)), os.getenv("DOCUMENT_PATH")))
 
-def create_controller():
+def get_docusign_credentials():
     """
-    1. Check the token
-    2. Call the worker method
+    1. Creates docusign credentials from .env file
     """
-    envelope_args = {
-        "signer_email": "",
-        "signer_name": "",
-        "cc_email": "",
-        "cc_name": "",
-        "status": "sent",
-    }
+
     args = {
-        "account_id": "",
-        "base_path": "",
-        "ds_access_token": "",
-        "envelope_args": envelope_args
+        "account_id": os.getenv("ACCOUNT_ID"),
+        "base_path": os.getenv("BASE_PATH"),
+        "ds_access_token": os.getenv("DS_ACCESS_TOKEN"),
     }
-
-    results = worker(args)
-    print(results)
-
-# ***DS.snippet.0.start
-def worker(args):
+    return args
+    
+def send_slip_worker(cred_info, env_info, document_name):
     """
     1. Create the envelope request object
     2. Send the envelope
     """
-    envelope_args = args["envelope_args"]
     # 1. Create the envelope request object
-    envelope_definition = make_envelope(envelope_args)
+    envelope_definition = make_envelope(env_info, document_name)
 
     # 2. call Envelopes::create API method
     # Exceptions will be caught by the calling function
     api_client = ApiClient()
-    api_client.host = args["base_path"]
-    api_client.set_default_header("Authorization", "Bearer " + args["ds_access_token"])
+    api_client.host = cred_info["base_path"]
+    api_client.set_default_header("Authorization", "Bearer " + cred_info["ds_access_token"])
 
     envelopes_api = EnvelopesApi(api_client)
-    results = envelopes_api.create_envelope(args["account_id"], envelope_definition=envelope_definition)
-
-    envelope_id = results.envelope_id
-    
+    result = envelopes_api.create_envelope(cred_info["account_id"], envelope_definition=envelope_definition)
+    envelope_id = result.envelope_id
     return {"envelope_id": envelope_id}
 
-
-def make_envelope(args):
+def make_envelope(env_info, document_name):
     """
-    Creates envelope
-    Document 1: An HTML document.
-    Document 2: A Word .docx document.
-    Document 3: A PDF document.
-    DocuSign will convert all of the documents to the PDF format.
-    The recipients" field tags are placed using <b>anchor</b> strings.
+    Creates envelope with a given document.
+    Example env 
+    envelope_args = {
+        "signer_email":<email>,
+        "signer_name": <name>,
+        "status": <sent>,
+        "child_name": <child>
+    }
     """
-
-    # document 1 (html) has sign here anchor tag **signature_1**
-    # document 2 (docx) has sign here anchor tag /sn1/
-    # document 3 (pdf)  has sign here anchor tag /sn1/
-    #
-    # The envelope has two recipients.
-    # recipient 1 - signer
-    # recipient 2 - cc
-    # The envelope will be sent first to the signer.
-    # After it is signed, a copy is sent to the cc person.
-
     # create the envelope definition
     env = EnvelopeDefinition(
         email_subject="Please sign this document set"
     )
-    doc1_b64 = base64.b64encode(bytes(create_document1(args), "utf-8")).decode("ascii")
-    # read files 2 and 3 from a local directory
+   
     # The reads could raise an exception if the file is not available!
-
-    # with open(path.join(demo_docs_path, ds_config.DS_CONFIG["doc_pdf"]), "rb") as file:
-    #     doc3_pdf_bytes = file.read()
-    # doc3_b64 = base64.b64encode(doc3_pdf_bytes).decode("ascii")
+    with open(path.join(docs_path, document_name), "rb") as file:
+        doc_pdf_bytes = file.read()
+    doc_b64 = base64.b64encode(doc_pdf_bytes).decode("ascii")
 
     # Create the document models
-    document1 = Document(  # create the DocuSign document object
-        document_base64=doc1_b64,
-        name="Order acknowledgement",  # can be different from actual file name
-        file_extension="html",  # many different document types are accepted
+    document = Document(  # create the DocuSign document object
+        document_base64=doc_b64,
+        name="Lorem Ipsum",  # can be different from actual file name
+        file_extension="pdf",  # many different document types are accepted
         document_id="1"  # a label used to reference the doc
     )
-    # document2 = Document(  # create the DocuSign document object
-    #     document_base64=doc2_b64,
-    #     name="Battle Plan",  # can be different from actual file name
-    #     file_extension="docx",  # many different document types are accepted
-    #     document_id="2"  # a label used to reference the doc
-    # )
-    # document3 = Document(  # create the DocuSign document object
-    #     document_base64=doc3_b64,
-    #     name="Lorem Ipsum",  # can be different from actual file name
-    #     file_extension="pdf",  # many different document types are accepted
-    #     document_id="3"  # a label used to reference the doc
-    # )
     # The order in the docs array determines the order in the envelope
-    env.documents = [document1]
-
+    env.documents = [document]
 
     # Create the signer recipient model
-    signer1 = Signer(
-        email=args["signer_email"], name=args["signer_name"],
+    signer = Signer(
+        email= env_info["signer_email"], name=env_info["signer_name"] ,
         recipient_id="1", routing_order="1"
     )
-    # routingOrder (lower means earlier) determines the order of deliveries
-    # to the recipients. Parallel routing order is supported by using the
-    # same integer as the order for two or more recipients.
 
-    # create a cc recipient to receive a copy of the documents
-    cc1 = CarbonCopy(
-        email=args["cc_email"], name=args["cc_name"],
-        recipient_id="2", routing_order="2")
+    sign_here = SignHere(
+        anchor_string = "/sn1/", anchor_units = "pixels",
+        anchor_y_offset = "0", anchor_x_offset = "0")
 
-    # Create signHere fields (also known as tabs) on the documents,
-    # We're using anchor (autoPlace) positioning
-    #
-    # The DocuSign platform searches throughout your envelope"s
-    # documents for matching anchor strings. So the
-    # signHere2 tab will be used in both document 2 and 3 since they
-    # use the same anchor string for their "signer 1" tabs.
-    sign_here1 = SignHere(
-        anchor_string = "**signature_1**", anchor_units = "pixels",
+    parent_full_name = FullName(name= env_info["signer_name"],
+            anchor_string="/pn0/",anchor_units = "pixels",
         anchor_y_offset = "10", anchor_x_offset = "20")
-    # sign_here2 = SignHere(
-    #     anchor_string = "/sn1/", anchor_units = "pixels",
-    #     anchor_y_offset = "10", anchor_x_offset = "20")
+    
+    child_full_name = FullName(name=env_info["child_name"],
+            anchor_string="/cn0/",anchor_units = "pixels",
+        anchor_y_offset = "10", anchor_x_offset = "20")
 
-    # Add the tabs model (including the sign_here tabs) to the signer
-    # The Tabs object wants arrays of the different field/tab types
-    signer1.tabs = Tabs(sign_here_tabs=[sign_here1])
+    signer.tabs = Tabs(sign_here_tabs=[sign_here], full_name_tabs=[parent_full_name, child_full_name])
 
     # Add the recipients to the envelope object
-    recipients = Recipients(signers=[signer1], carbon_copies=[cc1])
+    recipients = Recipients(signers=[signer])
     env.recipients = recipients
 
     # Request that the envelope be sent by setting |status| to "sent".
-    # To request that the envelope be created as a draft, set to "created"
-    env.status = args["status"]
+    env.status = env_info["status"]
     return env
 
+def list_recipients(cred_info, envelope_id):
+    """
+    1. Call the envelope recipients list method
+    """
+    # Exceptions will be caught by the calling function
+    api_client = ApiClient()
+    api_client.host = cred_info["base_path"]
+    api_client.set_default_header("Authorization", "Bearer " + cred_info["ds_access_token"])
+    envelope_api = EnvelopesApi(api_client)
+    from_date = (datetime.utcnow() - timedelta(days=10)).isoformat()
+    results = envelope_api.list_status_changes(cred_info["account_id"], from_date = from_date)
 
-def create_document1(args):
-    """ Creates document 1 -- an html document"""
+    return results
 
-    return f"""
-    <!DOCTYPE html>
-    <html>
-        <head>
-          <meta charset="UTF-8">
-        </head>
-        <body style="font-family:sans-serif;margin-left:2em;">
-        <h1 style="font-family: "Trebuchet MS", Helvetica, sans-serif;
-            color: darkblue;margin-bottom: 0;">World Wide Corp</h1>
-        <h2 style="font-family: "Trebuchet MS", Helvetica, sans-serif;
-          margin-top: 0px;margin-bottom: 3.5em;font-size: 1em;
-          color: darkblue;">Order Processing Division</h2>
-        <h4>Ordered by {args["signer_name"]}</h4>
-        <p style="margin-top:0em; margin-bottom:0em;">Email: {args["signer_email"]}</p>
-        <p style="margin-top:0em; margin-bottom:0em;">Copy to: {args["cc_name"]}, {args["cc_email"]}</p>
-        <p style="margin-top:3em;">
-  Candy bonbon pastry jujubes lollipop wafer biscuit biscuit. Topping brownie sesame snaps sweet roll pie. Croissant danish biscuit soufflé caramels jujubes jelly. Dragée danish caramels lemon drops dragée. Gummi bears cupcake biscuit tiramisu sugar plum pastry. Dragée gummies applicake pudding liquorice. Donut jujubes oat cake jelly-o. Dessert bear claw chocolate cake gummies lollipop sugar plum ice cream gummies cheesecake.
-        </p>
-        <!-- Note the anchor tag for the signature field is in white. -->
-        <h3 style="margin-top:3em;">Agreed: <span style="color:white;">**signature_1**/</span></h3>
-        </body>
-    </html>
-  """
+if __name__ == "__main__":
+    cred_info = get_docusign_credentials()
+    env_info = {
+        "signer_email": os.getenv("SIGNER_EMAIL"),
+        "signer_name": os.getenv("SIGNER_NAME"),
+        "status": "sent",
+        "child_name": os.getenv("CHILD_NAME")
+    }
+    envelope_id = send_slip_worker(cred_info, env_info, document_name="example_doc.pdf")
+    print(envelope_id)
+    print(list_recipients(cred_info, envelope_id["envelope_id"]))
 
-create_controller()
+
