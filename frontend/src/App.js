9dac17c3a1c827ee899426fcc5a79fe0a1e0ec4b
Freniel <frenielzabala@gmail.com>
2019-10-27 08:25:27 -0700

    qwe

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,13 +12,13 @@ class PermissionSlipPrompt extends React.Component {
         selectedCsvFile: null,
       }
   }
+
   handleSubmit = (event) =>{
     event.preventDefault();
     const data = new FormData()
     const {selectedPdfFile, selectedCsvFile} = this.state
     data.append("permission_slip", this.state.selectedPdfFile)
     data.append("csv_file", this.state.selectedCsvFile)
-
     axios.post("http://localhost:5000/upload", data, { 
       // receive two    parameter endpoint url ,form data
     })
@@ -29,6 +29,11 @@ class PermissionSlipPrompt extends React.Component {
     })
   }
 
+  handleChange = (event) =>{
+    // check it out: we get the evt.target.name (which will be either "email" or "password")
+    // and use it to target the key on our `state` object with the same name, using bracket syntax
+    this.setState({ [event.target.name]: event.target.value });
+  }
   onChangeHandlerPDF = event =>{
     console.log(event.target.files)
     this.setState({
@@ -43,28 +48,16 @@ class PermissionSlipPrompt extends React.Component {
     })
   }
 
-//   onClickHandler = () => {
-//    const data = new FormData()
-//    data.append('file', this.state.selectedFile)
-//    axios.post("http://localhost:8000/upload", data, { 
-//       // receive two    parameter endpoint url ,form data
-//   })
-// }
-
-
-
   render() {
     return (
       <div className="prompt">
         <form onSubmit={this.handleSubmit}>
-          {/* <label className="prompt-label" for="name">Name</label> */}
-          {/* <input type="text" name="name"></input> */}
+          <label className="prompt-label" for="name">Name</label>
+          <input type="text" name="name" onChange={this.handleChange}></input>
           <label className="prompt-label" for="slip">Permission Slip (PDF)</label>
           <input type="file" className="form-control" name="file" accept=".pdf" onChange={this.onChangeHandlerPDF}></input>
           <label className="prompt-label" for="emails">Emails (CSV)</label>
           <input type="file" name="emails" accept=".csv" onChange={this.onChangeHandlerCSV}></input>
-          {/* <label className="prompt-label" for="due-date">Due Date</label> */}
-          {/* <input type="date" name="due-date"></input> */}
           <button type="submit">submit</button>
         </form>
       </div>
@@ -72,165 +65,50 @@ class PermissionSlipPrompt extends React.Component {
   }
 }
 
-class StudentTable extends React.Component {
+
+class Dashboard  extends React.Component {
   constructor() {
     super();
-    this.columns = React.useMemo(
-      () => [
-        {
-          Header: 'Name',
-          accessor: 'name'
-        },
-        {
-          Header: 'Signed',
-          accessor: 'signed'
-        },
-        {
-          Header: 'Due',
-          accessor: 'due'
-        }
-      ]
-    );
+    this.state = {
+      slips: []
+    }
   }
-  render() {
+
+  async componentDidMount() {
+    let data = await axios.get("http://localhost:5000/perm_slips").then(res => {
+      const slips = res.data.slips;
+      this.setState({ slips });
+    })
+  }
+
+  sendStuff = (slip_id) => {
+    console.log(slip_id)
+    axios.get("http://localhost:5000/perm_slips/".concat(slip_id))
+  }
+
+  render(){
+    const {slips} = this.state
+    const items = slips.map((data, index) => {return <tr><td>document {index + 1}</td><td><button id="addBtn"  onClick={() => this.sendStuff(data._id)}>Send</button></td>
+    </tr>})
     return (
-      <div></div>
-    );
+      <div>
+          <table>
+              {items}
+          </table>
+          {/* <button id="addBtn" onClick={addRow}>ADD</button> */}
+      </div>
+  );
   }
 }
 
-function Table({ columns, data }) {
-  const {
-    getTableProps,
-    getTableBodyProps,
-    headerGroups,
-    rows,
-    prepareRow,
-  } = useTable({
-    columns,
-    data
-  });
-
-  return (
-    <table {...getTableProps()}>
-      <thead>
-        {headerGroups.map(headerGroup => (
-          <tr {...headerGroup.getHeaderGroupProps()}>
-            {headerGroup.headers.map(column => (
-              <th {...column.getHeaderProps()}>{column.render('Header')}</th>
-            ))}
-          </tr>
-        ))}
-      </thead>
-      <tbody {...getTableBodyProps()}>
-        {rows.map(
-          (row, i) =>
-            prepareRow(row) || (
-              <tr {...row.getRowProps()}>
-                {row.cells.map(cell => {
-                  return (
-                    <td {...cell.getCellProps()}>
-                      {cell.render('Cell')}
-                    </td>
-                  );
-                })}
-              </tr>
-            )
-        )}
-      </tbody>
-    </table>
-  );
-}
-
-function Dashboard({ columns, data }) {
-  const {
-    getTableProps,
-    getTableBodyProps,
-    headerGroups,
-    rows,
-    prepareRow,
-  } = useTable({
-    columns,
-    data
-  });
-
-  return (
-    <table {...getTableProps()}>
-      <thead>
-        {headerGroups.map(headerGroup => (
-          <tr {...headerGroup.getHeaderGroupProps()}>
-            {headerGroup.headers.map(column => (
-              <th {...column.getHeaderProps()}>{column.render('Header')}</th>
-            ))}
-          </tr>
-        ))}
-      </thead>
-      <tbody {...getTableBodyProps()}>
-        {rows.map(
-          (row, i) =>
-            prepareRow(row) || (
-              <Popup trigger={
-                <tr {...row.getRowProps()}>
-                  {row.cells.map(cell => {
-                    return (
-                      <td {...cell.getCellProps()}>
-                        {cell.render('Cell')}
-                      </td>
-                    );
-                  })}
-                </tr>
-              } modal >
-                <Table />
-              </Popup>
-            )
-        )}
-      </tbody>
-    </table>
-  );
-}
 
 function App() {
-  const columns = React.useMemo(
-    () => [
-      {
-        Header: 'Name',
-        accessor: 'name'
-      },
-      {
-        Header: 'Signed',
-        accessor: 'signed'
-      },
-      {
-        Header: 'Due',
-        accessor: 'due'
-      }
-    ]
-  );
-
-  const data = [
-    {
-      "name": "Field Trip Permission Slip",
-      "signed": "33%",
-      "due": "10/30"
-    },
-    {
-      "name": "Student-Parent Photo Consent Form",
-      "signed": "100%",
-      "due": "10/25"
-    },
-    {
-      "name": "Winter 2019 Syllabus",
-      "signed": "25%",
-      "due": "11/5"
-    }
-  ];
-
   return (
     <div>
       <Popup trigger={<button className="button">new</button>} modal>
         <PermissionSlipPrompt />
       </Popup>
-      <Dashboard columns={columns} data={data} />
+      <Dashboard />
     </div>
   );
 }
